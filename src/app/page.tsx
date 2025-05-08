5276670803d8a320d94fe5de83900d1b04b2b201
Ehtisham Aslam <ehtisham.ea10@gmail.com>
2025-05-08 05:26:24 -0700

    customization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,30 @@
 import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <Button>Hello World</Button>
+    <div className="p-4">
+      <div className="flex flex-col gap-y-4">
+        <div>
+          <Button variant="elevated">I am a Button</Button>
+        </div>
+        <div>
+          <Input placeholder="I am an Input" />
+        </div>
+        <div>
+          <Progress value={50} />
+        </div>
+        <div>
+          <Textarea placeholder="I am a Textarea" />
+        </div>
+        <div>
+          <Checkbox />
+        </div>
+      </div>
     </div>
   );
 }
